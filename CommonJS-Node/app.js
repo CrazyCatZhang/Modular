fabf9bdc06f050c3cd8cc5dac1367b5f886059d4
Peach Zhang <CrazyCatZhang@outlook.com>
2022-04-08 09:50:40 +0800

    Node-based server-side CommonJS specification

---
+++ CommonJS-Node/app.js
@@ -0,0 +1,7 @@
+const modele1 = require('./modules/module1');
+const modele2 = require('./modules/module2');
+const modele3 = require('./modules/module3');
+
+console.log(modele1.getMsg());
+modele2();
+modele3.foo();
